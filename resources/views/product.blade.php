f0881c228835be2ed8ce431deb27a44f47c2eb5d
VTNQ <votannamquoc@gmail.com>
2023-06-18 13:51:06 +0700

    update

---
+++ resources/views/product.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.template')
+
+@section('title_page')
+
+home page
+
+@endsection
+
+@section('mycss')
+
+@endsection
+
+@section('body_content')
+
+<h1>welcome to products</h1>
+
+@endsection
+
+@section('myscript')
+
+@endsection
